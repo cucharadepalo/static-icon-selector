247c4547fe2c302d4154373cd31a6a07f80e29d1
Abraham Castro <cucharadepalo@gmail.com>
2020-03-11 21:28:53 +0100

    Styling

--- app.js
+++ app.js
@@ -1,44 +1,56 @@
 window.addEventListener("load", () => {
-  let app, parser;
-      app = document.getElementById("app"),
-      parser = new DOMParser();
+    let iconGrid = document.getElementById("iconGrid"),
+        toolbar = document.getElementById("toolbar"),
+        parser = new DOMParser();
 
-  function groupBy(xs, key) {
-      return xs.reduce((rv, x) => {
-          (rv[x[key]] = rv[x[key]] || []).push(x);
-          return rv;
-      }, {});
-  };
 
-  const data = "iconos/icons.json";
-  let request = new XMLHttpRequest();
-      request.open("GET", data);
-      request.responseType = "json";
-      request.send();
-      request.onload = () => {
-          let icons = request.response;
-          generateUI(groupBy(icons, 'category'));
-      }
 
-  function generateUI(iconsByCat) {
-      Object.keys(iconsByCat).forEach(category => {
-          let container, heading, category_icons;
-              container = document.createElement("div");
-              container.className = "grid";
-              container.id = category.toLowerCase();
-              heading = document.createElement("h2");
-              heading.innerHTML = category == 'none' ? 'uncategorized': category;
-              category_icons = iconsByCat[category];
+    const data = "iconos/icons.json";
+    let request = new XMLHttpRequest();
+    request.open("GET", data);
+    request.responseType = "json";
+    request.send();
+    request.onload = () => {
+        let icons = request.response;
+        generateUI(groupBy(icons, 'category'));
+    }
 
-          category_icons.forEach(element => {
-              let wrapper = document.createElement("button");
-                  wrapper.className = "icon";
-                  wrapper.id = element["name"];
-              let svg = parser.parseFromString(element["icon"], "image/svg+xml");
-                  wrapper.appendChild(svg.documentElement);
-                  container.appendChild(wrapper);
-          });
-          app.append(heading, container);
-      });
-  }         
+    function generateUI(iconsByCat) {
+        Object.keys(iconsByCat).forEach(category => {
+            let container = document.createElement("div");
+            container.className = "grid";
+            container.id = category.toLowerCase();
+            let heading = document.createElement("h2");
+            heading.className = "category-heading";
+            heading.innerHTML = category == 'none' ? 'uncategorized' : category;
+            let category_icons = iconsByCat[category];
+
+            category_icons.forEach(element => {
+                let button = document.createElement("button");
+                button.className = "icon-btn";
+                button.id = element["name"];
+                button.setAttribute("data-filename", element["filename"]);
+                button.addEventListener("click", showDetail);
+                let svg = parser.parseFromString(element["icon"], "image/svg+xml");
+                button.appendChild(svg.documentElement);
+                container.appendChild(button);
+            });
+            iconGrid.append(heading, container);
+        });
+    }
+
+    function showDetail(e) {
+        let icon = e.currentTarget;
+        let preview = toolbar.querySelector(".icon-details");
+        let codeBlock = toolbar.querySelector("pre");
+        preview.innerHTML = icon.firstElementChild.outerHTML;
+        codeBlock.textContent = icon.firstElementChild.outerHTML;
+    }
+
+    function groupBy(xs, key) {
+        return xs.reduce((rv, x) => {
+            (rv[x[key]] = rv[x[key]] || []).push(x);
+            return rv;
+        }, {});
+    }
 });
